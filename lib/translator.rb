18830307c20c1f74c246f29aeaad88d35e260bfa
Scot Sexton <rss3509@gmail.com>
2020-05-23 01:20:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
               
                   'get_emoticon' => {} }
 
-  emoticons.each do |meaning, value|  
+    emoticons.each do |meaning, value|  
     english = value[0]
     japanese = value[1]
     emoticon_lib['get_meaning'][japanese] = meaning
