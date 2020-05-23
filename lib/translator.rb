db29bed15c82e7736a54cdd5045ff6231d546854
Scot Sexton <rss3509@gmail.com>
2020-05-23 00:42:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(/lib/emoticons.yml)
 
   emoticon_lib = {'get_meaning'  => {},
+              
                   'get_emoticon' => {} }
 puts emoticons
   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
